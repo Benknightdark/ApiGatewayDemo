eb9171ceab4410ad3b47cee7e16e883a4ed0d4a6
Ben <moon791017@gmail.com>
2020-08-20 09:00:26 +0800

    OK

---
+++ api1/Controllers/Service1Controller.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace api1.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class Service1Controller : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<Service1Controller> _logger;
+
+        public Service1Controller(ILogger<Service1Controller> logger)
+        {
+            _logger = logger;
+        }
+
+       [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+           return Ok("from service2");
+        }
+    }
+}
---
+++ api2/Controllers/Service2Controller.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace api2.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class Service2Controller : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<Service2Controller> _logger;
+
+        public Service2Controller(ILogger<Service2Controller> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+           return Ok("from service2");
+        }
+    }
+}
